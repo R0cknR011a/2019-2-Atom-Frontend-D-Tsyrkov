a3bf5ec79ed55886a75f8a964c07cdf707db27ac
RocknRolla <myemail@sobaka.com>
2019-09-28 10:10:04 +0300

    Deploy script was executed

---
+++ build/precache-manifest.7401316a3daf3a6c997c99efea1aea57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a335f30cd31b4a207e46346a749c04",
+    "url": "/2019-1-Atom-Frontend-D-Tsyrkov/index.html"
+  },
+  {
+    "revision": "e12ae7a8a153371fac16",
+    "url": "/2019-1-Atom-Frontend-D-Tsyrkov/static/css/main.0a628ba3.chunk.css"
+  },
+  {
+    "revision": "93931eed4bed14aa9b4d",
+    "url": "/2019-1-Atom-Frontend-D-Tsyrkov/static/js/2.932d236f.chunk.js"
+  },
+  {
+    "revision": "e12ae7a8a153371fac16",
+    "url": "/2019-1-Atom-Frontend-D-Tsyrkov/static/js/main.f8f3a006.chunk.js"
+  },
+  {
+    "revision": "985aa81b022cad0ea4b3",
+    "url": "/2019-1-Atom-Frontend-D-Tsyrkov/static/js/runtime-main.d8b5db36.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/2019-1-Atom-Frontend-D-Tsyrkov/static/media/logo.a55f1c43.svg"
+  }
+]);
