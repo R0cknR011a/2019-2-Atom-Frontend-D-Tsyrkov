2c8af16c27e8043e1132ee5f682a408629df14aa
RocknRolla <favorite.vanity@yandex.ru>
2019-10-11 21:56:09 +0300

    ESlint fix v0.4

--- web-components/src/components/FormInput.js
+++ web-components/src/components/FormInput.js
@@ -1,4 +1,4 @@
-const template = document.createElement('template')
+const template = document.createElement('template');
 template.innerHTML = `
     <style>
         input {
@@ -20,32 +20,32 @@ template.innerHTML = `
         
     </style>
     <input type="text">
-`
+`;
 
 class FormInput extends HTMLElement {
     constructor () {
-        super()
-        this._shadowRoot = this.attachShadow({ mode: 'open' })
-        this._shadowRoot.appendChild(template.content.cloneNode(true))
+        super();
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-        this.$input = this._shadowRoot.querySelector('input')
+        this.$input = this._shadowRoot.querySelector('input');
     }
-
+    
     static get observedAttributes() {
-        return ['name', 'value', 'placeholder', 'disabled']
+        return ['name', 'value', 'placeholder', 'disabled'];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-        this.$input.setAttribute(name, newValue)
+        this.$input.setAttribute(name, newValue);
     }
 
     get value() {
-        return this.$input.value
+        return this.$input.value;
     }
 
     set value(newValue) {
-        this.$input.value = newValue
+        this.$input.value = newValue;
     }
 }
 
-customElements.define('form-input', FormInput)
+customElements.define('form-input', FormInput);
